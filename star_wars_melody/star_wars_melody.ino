649b8c6c352d1e285de75da7ba2f75842fb2b4ff
Prof Tallman <joshua.tallman@cui.edu>
2026-01-13 08:50:27 -0800

    fixed non-blocking bug

--- star_wars_melody/star_wars_melody.ino
+++ star_wars_melody/star_wars_melody.ino
@@ -24,6 +24,7 @@
 #define LENGTH_OF(x)  sizeof(x) / sizeof(*x)
 
 // musical constants
+#define SPEAKER_PIN 8
 int bpm = 95;
 int time_signature = 4;
 
@@ -52,12 +53,27 @@ void play_melody()
   }
 
   // Play the melody one note at a time
-  int measure_time = 60.0 / bpm * 1000 * time_signature;
+  int measure_time = 1000 * time_signature * 60.0 / bpm;
+  Serial.print("Time Signature:   ");
+  Serial.println(time_signature);
+  Serial.print("Beats per Minute: ");
+  Serial.println(bpm);  
+  Serial.print("Measure time:     ");
+  Serial.println(measure_time);
   for (int idx = 0; idx < LENGTH_OF(notes); idx++) 
   {
+    int note_tone = notes[idx];
     int note_time = 0.9 * measure_time / beats[idx];
     int rest_time = 0.1 * measure_time / beats[idx];
-    tone(SPEAKER_PIN, notes[idx], note_time);
+    Serial.print("Note: ");
+    Serial.print(note_tone);
+    Serial.print(" Hz for ");
+    Serial.print(note_time);
+    Serial.print(" milliseconds");
+    Serial.println();
+    tone(SPEAKER_PIN, note_tone, note_time);
+    delay(note_time);
+    noTone(SPEAKER_PIN);
     delay(rest_time);
   }
 
@@ -66,7 +82,8 @@ void play_melody()
 
 void setup()
 {
-  play_melody()
+  Serial.begin(9600);
+  play_melody();
 }
 
 void loop()
